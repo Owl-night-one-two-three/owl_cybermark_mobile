446ddd293e873d7da27b60f621a2c4f73c2d65a5
NamKingBo <130020971+NamKingBo@users.noreply.github.com>
2023-08-06 11:21:16 +0700

    Config Eslint (#3)
    - [ ] add some implement into file .eslintrc.js

--- CyberMart/.eslintrc.js
+++ CyberMart/.eslintrc.js
@@ -1,21 +1,85 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
-    'jest/globals': true,
+    browser: true,
+    node: true,
   },
-  root: true,
-  extends: '@react-native',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'react-hooks',
+    'react-native',
+    'import',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:import/errors',
+    // 'plugin:import/warnings',
+    'prettier',
+  ],
   rules: {
-    'react-hooks/exhaustive-deps': 'off',
-    quotes: ['error', 'single'],
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'never'],
-    'react/require-default-props': ['error'],
-    'react/default-props-match-prop-types': ['error'],
-    'react/sort-prop-types': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        endOfLine: 'auto',
+        jsxSingleQuote: true,
+      },
+    ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars":  ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    // this is for sorting WITHIN an import
+    'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
+    // this is for sorting imports
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
   settings: {
-    'import/resolver': {
-      'babel-module': {},
+    react: {
+      version: 'detect',
     },
   },
 };
